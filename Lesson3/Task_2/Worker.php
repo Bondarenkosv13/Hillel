905b6ac5d318a0f908bccb6eb598051477627306
Bondarenkosv <bondarenkosv13@gmail.com>
2020-03-24 21:57:17 +0200

    delete methods

--- Lesson3/Task_2/Worker.php
+++ Lesson3/Task_2/Worker.php
@@ -5,33 +5,19 @@ class Worker
     public $age;
     public $salary;
 
-    public function name($name)
-    {
-        return $name;
-    }
-
-    public function age($age)
-    {
-        return $age;
-    }
-
-    public function salary($salary)
-    {
-        return $salary;
-    }
 
 }
 
 $John       = new Worker();
-$J_name     = $John->name ('John');
-$J_age      =$John->age('25');
-$J_salary   =$John->salary('1000');
+$J_name     = $John->name   = "John";
+$J_age      = $John->age    = '25';
+$J_salary   = $John->salary = '1000';
 
 
 $Vasia      = new Worker();
-$V_name     = $Vasia->name ('Вася');
-$V_age      =$Vasia->age('26');
-$V_salary   =$Vasia->salary('2000');
+$V_name     = $Vasia->name  ='Вася';
+$V_age      = $Vasia->age    = '26';
+$V_salary   = $Vasia->salary = '2000';
 
 $sum_salary = $J_salary + $V_salary;
 $sum_age    = $J_age + $V_age;
