eae8410cc99675b08d081df2d9b6dc30b071b68e
Bondarenkosv <bondarenkosv13@gmail.com>
2020-03-24 21:51:15 +0200

    In AUser.php add properties

--- Lesson3/Task_4/AUser.php
+++ Lesson3/Task_4/AUser.php
@@ -10,69 +10,122 @@ abstract class AUser
 }
 class Employee extends AUser
 {
+    private $AverageMark = null;
+    public $Age;
+    public $Pay;
+
     public function name()
     {
         return $this->name;
     }
 
     // выслуга лет
-    public function WorkAge()
+    public function Age()
     {
-
-    };
+        return $this->Age;
+    }
+    public function Pay ()
+    {
+        return $this->Pay;
+    }
 
     public function increaseRevenue()
     {
 
+        $increaseRevenue = $this->Pay + ($this->Pay * $this->Age / 2);
+        return $increaseRevenue;
     }
 
 
 }
 class Student extends AUser
 {
-    private $AverageMark;
-    private $StudyAge;
-    private $StudyPay;
-    private $increaseRevenue;
+    public $AverageMark;
+    public $Age;
+    public $Pay;
+
 
     public function name()
     {
         return $this->name;
     }
     // количество лет учебы
-    public function StudyAge($StudyAge)
+    public function Age()
     {
-        if ($StudyAge > 5)
+        if ($this->Age > 5)
         {
             echo "Студент не может учиться больше 5 лет!";
+
+            $this->Age = null;
+            $this->Pay = null;
+            $this->AverageMark = 0;
+
         }
+
         else
         {
-            return $StudyAge;
+            return $this->Age;
         }
     }
     // средний бал студента
-    public function AverageMark($AverageMark)
+    public function AverageMark()
     {
-        if ($AverageMark > 10)
+        if ($this->AverageMark > 10)
         {
             echo "Самый большой бал 10";
+
+            $this->Age = null;
+            $this->Pay = null;
+            $this->AverageMark = 0;
         }
         else
         {
-            return $AverageMark;
+            return $this->AverageMark;
         }
     }
 
-    public function StudyPay ()
+    public function Pay ()
     {
-        return $this->StudyPay;
+        return $this->Pay;
     }
 
     public function increaseRevenue()
     {
-        $increaseRevenue = $this->StudyPay * ($this->AverageMark * $this->StudyAge / 100) + $this->StudyPay;
+        $increaseRevenue = $this->Pay * ($this->AverageMark * $this->Age / 100) + $this->Pay;
         return $increaseRevenue;
     }
 }
+
+    $Bob = new Student();
+    $Bob->name = "Bob";
+    $Bob->Age = "6";
+    $Bob->AverageMark = "7";
+    $Bob->Pay = "2500";
+
+    echo "Студент: ";
+    Print_r($Bob->name());
+        echo "<br> Курс студента:   ";
+    Print_r($Bob->Age());
+        echo "<br> Средний бал студента:   ";
+    Print_r($Bob->AverageMark());
+        echo " из 10<br>Начальная степендия студента:   ";
+    Print_r($Bob->Pay());
+        echo "<br>Степендия студена на данный момент:   ";
+    Print_r($Bob->increaseRevenue());
+
+    echo "<hr><br>";
+
+    $Sam = new Employee();
+    $Sam->name = "Sam";
+    $Sam->Age   = '7';
+    $Sam->Pay   = '4000';
+
+    echo "Сотрудник: ";
+    Print_r($Sam->name());
+    echo "<br> Стаж сотрудника: ";
+    Print_r($Sam->Age());
+    echo "<br>Начальная зарплата сотрудника: ";
+    Print_r($Sam->Pay());
+    echo "<br>Степендия студена на данный момент: ";
+    Print_r($Sam->increaseRevenue());
 
