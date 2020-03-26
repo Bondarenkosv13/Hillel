6ad0c9d38ff6d4f4553057cc4e9f1b9602f2fdbb
Bondarenkosv <bondarenkosv13@gmail.com>
2020-03-26 11:42:44 +0200

    Finish home work lesson 4, Task 1

---
+++ Lesson4/Task_1/Trait/Trait3.php
@@ -0,0 +1,10 @@
+<?php
+
+trait Trait3
+{
+    public function method()
+    {
+        $a = 3;
+        return $a;
+    }
+}
---
+++ Lesson4/Task_1/index.php
@@ -0,0 +1,27 @@
+<?php
+
+include_once 'Trait/Trait1.php';
+include_once 'Trait/Trait2.php';
+include_once 'Trait/Trait3.php';
+
+class Test
+{
+    use Trait1, Trait2, Trait3
+    {
+        Trait1::method insteadof Trait2;
+        Trait1::method insteadof Trait3;
+        Trait2::method as Trait2;
+        Trait3::method as Trait3;
+    }
+
+    public function getSum ()
+    {
+        $sum = $this->method() + $this->Trait3() + $this->Trait2();
+        return $sum;
+    }
+
+}
+
+$a = new Test ();
+echo $a -> getSum();
+
