47c8a3a63617dcb5e4ee4ff1d4d0613f112eb15e
Bondarenkosv <bondarenkosv13@gmail.com>
2020-04-11 10:33:36 +0300

    Add Task $13

---
+++ Lesson9/Task_1/index.php
@@ -0,0 +1,19 @@
+<?php
+class Product
+{
+    public function set(WorkProduct $name, WorkProduct $value) {}
+    public function get(PrintProduct $name) {}
+}
+
+Class WorkProduct
+{
+    public function save() {}
+    public function update() {}
+    public function delete() {}
+    public function show() {}
+}
+
+Class PrintProduct
+{
+    public function print() {}
+}
